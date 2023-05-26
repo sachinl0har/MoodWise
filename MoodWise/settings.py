f6ddbe872e41363a069588b713050ef75b1e3f71
sachinl0har <sachin.l0har0424@gmail.com>
2023-05-26 19:09:45 +0530

    STATICFILES_STORAGE Commented

--- MoodWise/settings.py
+++ MoodWise/settings.py
@@ -137,7 +137,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 django_heroku.settings(locals())
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
