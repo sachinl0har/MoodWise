d465e67a984334f87e90b51a5eac1c2ff6a1e409
sachinl0har <sachin.l0har0424@gmail.com>
2023-08-04 23:59:11 +0530

    added password and confirm password

--- movie/forms.py
+++ movie/forms.py
@@ -5,7 +5,7 @@ from .models import User
 class MyUserCreationForm(UserCreationForm):
     class Meta:
         model = User
-        fields = ['name', 'username', 'email', 'password']
+        fields = ['name', 'username', 'email', 'password1', 'password2']
 
 class UserForm(ModelForm):
     class Meta:
